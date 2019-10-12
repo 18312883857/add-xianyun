6bfbde9418490808db7383ae4386b9be4e68ce41
砥砺前行 <1531012257@qq.com>
2019-10-12 09:26:49 +0800

    闲云主页与轮播图完成

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,7 +26,7 @@ export default {
   */
   css: [
     'element-ui/lib/theme-chalk/index.css',
-    'assets/min.css'
+    'assets/main.css'
   ],
   /*
   ** Plugins to load before mounting the App
--- pages/index.vue
+++ pages/index.vue
@@ -12,6 +12,28 @@
         ></div>
       </el-carousel-item>
     </el-carousel>
+    <!-- 搜索框 -->
+    <div class="banner-content">
+      <div class="search-bar">
+        <!-- tab栏 -->
+        <el-row type="flex" class="search-tab">
+          <span
+            v-for="(item,index) in options"
+            :key="index"
+            @click="headClick(index)"
+            :class="{active:index===current}"
+          >
+            <i>{{item.title}}</i>
+          </span>
+        </el-row>
+
+        <!-- 输入框 -->
+        <el-row type="flex" align="middle" class="search-input">
+          <input :placeholder="options[current].placeholder" />
+          <i class="el-icon-search"></i>
+        </el-row>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -20,17 +42,36 @@ export default {
   data() {
     return {
       // 轮播图数据
-      banners: []
+      banners: [],
+      // tab栏数据
+      options: [
+        { title: "攻略", placeholder: "搜索城市" },
+        { title: "酒店", placeholder: "请输入城市搜索酒店" },
+        { title: "机票", placeholder: "" }
+      ],
+      // tab栏的索引
+      current: 0
     };
   },
+  methods: {
+    headClick(index) {
+      console.log(123);
+      this.current = index;
+      if (index === 2) {
+        this.$router.push("/air");
+      }
+    }
+  },
   mounted() {
+    // console.log(123);
     this.$axios({
+      method: "get",
       url: "/scenics/banners"
     }).then(res => {
-      console.log(res)
+      console.log(res);
       const { data } = res.data;
       this.banners = data;
-      console.log(this.banners)
+      console.log(this.banners);
     });
   }
 };
@@ -41,14 +82,92 @@ export default {
   min-width: 1000px;
   margin: 0 auto;
   position: relative;
-
   /deep/ .el-carousel__container {
     height: 700px;
   }
-
   .banner-image {
     width: 100%;
     height: 100%;
+  }
+  .banner-content {
+    z-index: 9;
+    width: 1000px;
+    position: absolute;
+    left: 50%;
+    top: 45%;
+    margin-left: -500px;
+    border-top: 1px transparent solid;
+    .search-bar {
+      width: 552px;
+      margin: 0 auto;
+    }
+    .search-tab {
+      .active {
+        i {
+          color: #333;
+        }
+        &::after {
+          background: #eee;
+        }
+      }
+      span {
+        width: 82px;
+        height: 36px;
+        display: block;
+        position: relative;
+        margin-right: 8px;
+        cursor: pointer;
+        i {
+          position: absolute;
+          z-index: 2;
+          display: block;
+          width: 100%;
+          height: 100%;
+          line-height: 30px;
+          text-align: center;
+          color: #fff;
+        }
+        &:after {
+          position: absolute;
+          left: 0;
+          top: 0;
+          display: block;
+          content: "";
+          width: 100%;
+          height: 100%;
+          border: 1px rgba(255, 255, 255, 0.2) solid;
+          border-bottom: none;
+          transform: scale(1.1, 1.3) perspective(0.7em) rotateX(2.2deg);
+          transform-origin: bottom left;
+          background: rgba(0, 0, 0, 0.5);
+          border-radius: 1px 2px 0 0;
+          box-sizing: border-box;
+        }
+      }
+    }
+    .search-input {
+      width: 550px;
+      height: 46px;
+      background: #fff;
+      border-radius: 0 4px 4px 4px;
+      border: 1px rgba(255, 255, 255, 0.2) solid;
+      border-top: none;
+      box-sizing: unset;
+      input {
+        flex: 1;
+        height: 20px;
+        padding: 13px 15px;
+        outline: none;
+        border: 0;
+        font-size: 16px;
+      }
+      .el-icon-search {
+        cursor: pointer;
+        font-size: 22px;
+        padding: 0 10px;
+        font-weight: bold;
+      }
+    }
   }
 }
 </style>
