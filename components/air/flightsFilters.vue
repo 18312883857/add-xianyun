02129d9a21d453399c64ecc4d6e23f2901b2ac0f
砥砺前行 <1531012257@qq.com>
2019-10-15 21:58:01 +0800

    实现列表过滤功能

--- components/air/flightsFilters.vue
+++ components/air/flightsFilters.vue
@@ -39,7 +39,12 @@
       </el-col>
       <el-col :span="4">
         <el-select size="mini" v-model="airSize" placeholder="机型" @change="handleAirSize">
-          <el-option :label="item.name" :value="item.size" v-for="(item,index) in airSizeOptions" :key="index"></el-option>
+          <el-option
+            :label="item.name"
+            :value="item.size"
+            v-for="(item,index) in airSizeOptions"
+            :key="index"
+          ></el-option>
         </el-select>
       </el-col>
     </el-row>
@@ -61,29 +66,62 @@ export default {
       flightTimes: "", // 出发时间
       company: "", // 航空公司
       // 机型大小
-      airSize : "",
+      airSize: "",
       airSizeOptions: [
-          {name:'大',size:'L'},
-          {name:'中',size:'M'},
-          {name:'小',size:'S'}
-      ] 
+        { name: "大", size: "L" },
+        { name: "中", size: "M" },
+        { name: "小", size: "S" }
+      ]
     };
   },
   methods: {
     // 选择机场时候触发
-    handleAirport(value) {},
-
+    handleAirport(value) {
+      // 过滤数组
+      let arr = this.data.flights.filter(v => {
+        return v.org_airport_name == value;
+      });
+      this.$emit("setdatalist", arr);
+    },
     // 选择出发时间时候触发
-    handleFlightTimes(value) {},
+    handleFlightTimes(value) {
+        // value是当前选取起飞时间 将字符串截取出来
+        let [from,to] = value.split(',')
+        let arr = this.data.flights.filter(v=>{
+            // // 出发的时间是小时
+            let start = +v.dep_time.split(':')[0] 
+            // 如果条件不成立，则表明没有该航班 
+            return start >= from && start < to
+        })
+      this.$emit("setdatalist", arr);
+    },
 
     // 选择航空公司时候触发
-    handleCompany(value) {},
+    handleCompany(value) {
+        let arr = this.data.flights.filter(v=>{
+            return v.airline_name == value
+        })
+      this.$emit("setdatalist", arr);
+    },
 
     // 选择机型时候触发
-    handleAirSize(value) {},
+    handleAirSize(value) {
+        let arr = this.data.flights.filter(v=>{
+            return v.plane_size == value
+        })
+      this.$emit("setdatalist", arr);
+    },
 
     // 撤销条件时候触发
-    handleFiltersCancel() {}
+    handleFiltersCancel() {
+        // 清空输入框里面的值
+        this.airport = ''
+        this.flightTimes = ''
+        this.company = ''
+        this.airSize = ''
+        // 传递原数组
+        this.$emit('setdatalist',this.data.flights)
+    }
   }
 };
 </script>
--- pages/air/flights.vue
+++ pages/air/flights.vue
@@ -4,9 +4,9 @@
       <!-- 顶部过滤列表 -->
       <div class="flights-content">
         <!-- 过滤条件 -->
-        <FlightsFilters :data="flightsData"></FlightsFilters>
+        <FlightsFilters :data="cacheFlightsData" @setdatalist="setDataList"></FlightsFilters>
         <!-- 航班头部布局 -->
-        <FlightsListHead ></FlightsListHead>
+        <FlightsListHead></FlightsListHead>
         <!-- 航班信息 -->
         <FlightsItem v-for="(item,index) in dataList" :key="index" :data="item"></FlightsItem>
         <!-- 分页器 -->
@@ -38,20 +38,29 @@
 // import moment from "moment";
 import FlightsListHead from "@/components/air/flightsListHead";
 import FlightsItem from "@/components/air/flightsItem";
-import FlightsFilters from "@/components/air/flightsFilters"
+import FlightsFilters from "@/components/air/flightsFilters";
 export default {
   data() {
+    // .过滤条件触发时候需要修改数组列表flightsData.flights，
+    // 这样原来的列表就会被覆盖了，所以需要缓存一份列表用于根据条件提取数据。
+    // 要实现过滤条件，准备一份旧的数据缓存 准备一份过滤后的数据
+    // 也就是说，我们需要通过请求得到另一份数据，然后存储起来
     return {
       // 航班总数据 初始化数据  需要把整个对象传到过滤组件中
       flightsData: {
         flights: [],
-        info : {},
-        options : {}
-      }, 
-      
+        info: {},
+        options: {}
+      },
       pageIndex: 1,
       pageSize: 5,
-      laoding: true
+      laoding: true,
+      // 准备的一份对象用来缓存数据
+      cacheFlightsData: {
+        flights: [],
+        info: {},
+        options: {}
+      }
     };
   },
   components: {
@@ -60,30 +69,46 @@ export default {
     FlightsFilters
   },
   mounted() {
-    this.$axios({
-      url: "/airs",
-      params: this.$route.query
-    }).then(res => {
-      // 接收总数据
-      this.flightsData = res.data;
-      console.log(this.flightsData);
-      // 第一页的数据
-      // this.dataList = this.flightsData.flights.slice(0, this.pageSize);
-      this.laoding = false;
-    });
+    this.getData()
   },
-  computed:{
+  computed: {
     // 从flights总列表数据中切割出来数组列表
-      dataList(){
-        let arr = this.flightsData.flights.slice((this.pageIndex - 1) * this.pageSize,this.pageIndex * this.pageSize);
-        return arr
-      }
+    dataList() {
+      let arr = this.flightsData.flights.slice(
+        (this.pageIndex - 1) * this.pageSize,
+        this.pageIndex * this.pageSize
+      );
+      return arr;
+    }
   },
   methods: {
+    getData() {
+      this.$axios({
+        url: "/airs",
+        params: this.$route.query
+      }).then(res => {
+        // 接收总数据
+        //  // 而flightsData会被修改
+        this.flightsData = res.data;
+        // / 缓存一份新的列表数据数据，这个列表不会被修改
+        this.cacheFlightsData = { ...res.data };
+        // 第一页的数据
+        // this.dataList = this.flightsData.flights.slice(0, this.pageSize);
+        this.laoding = false;
+      });
+    },
+    // arr是用来展示的新数据，该方法将会传递给过滤组件使用
+    setDataList(arr){
+      console.log(arr)
+      if(!arr) return
+      // 如果有数据，从第一页开始显示
+      this.pageSize = 1
+      this.flightsData.flights = arr
+      this.flightsData.total = arr.length
+    },
     // 分页条数切换时候触发, val是当前的条数
     handleSizeChange(val) {
       this.pageSize = val;
-      this.pageIndex = 1
     },
     // 页数切换时候触发, val是当前的页数
     handleCurrentChange(val) {
