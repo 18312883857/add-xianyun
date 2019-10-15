1047c43fab1e79f45737ead62ecbe5137e814dce
砥砺前行 <1531012257@qq.com>
2019-10-15 18:56:21 +0800

    使用computed计算并监听分页数据

--- pages/air/flights.vue
+++ pages/air/flights.vue
@@ -44,11 +44,11 @@ import FlightsItem from "@/components/air/flightsItem";
 export default {
   data() {
     return {
+      // 航班总数据
       flightsData: {
         flights: []
-      }, // 航班总数据
-      // 从flights总列表数据中切割出来数组列表
-      dataList: [],
+      }, 
+      
       pageIndex: 1,
       pageSize: 5,
       laoding: true
@@ -65,30 +65,29 @@ export default {
     }).then(res => {
       // 接收总数据
       this.flightsData = res.data;
-      console.log(this.flightsData);
+      // console.log(this.flightsData);
       // 第一页的数据
-      this.dataList = this.flightsData.flights.slice(0, this.pageSize);
+      // this.dataList = this.flightsData.flights.slice(0, this.pageSize);
       this.laoding = false;
     });
   },
+  computed:{
+    // 从flights总列表数据中切割出来数组列表
+      dataList(){
+        let arr = this.flightsData.flights.slice((this.pageIndex - 1) * this.pageSize,this.pageIndex * this.pageSize);
+        return arr
+      }
+  },
   methods: {
-    // 封装切换数据的函数
-    setDataList() {
-      // 利用pageIndex和pageSize来切换页数
-      // 第一页 0，5 第二页 5，10 第三页 10，15
-      this.dataList = this.flightsData.flights.slice((this.pageIndex - 1) * this.pageSize,this.pageIndex * this.pageSize);
-    },
     // 分页条数切换时候触发, val是当前的条数
     handleSizeChange(val) {
       this.pageSize = val;
       this.pageIndex = 1
-      this.setDataList()
     },
     // 页数切换时候触发, val是当前的页数
     handleCurrentChange(val) {
       // 当前切换的页数
       this.pageIndex = val;
-      this.setDataList()
     }
   }
 };
