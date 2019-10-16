f622cd5931c962741ed2b443099d30444d63d794
砥砺前行 <1531012257@qq.com>
2019-10-16 18:32:51 +0800

    渲染侧边栏列表

--- components/air/flightsAside.vue
+++ components/air/flightsAside.vue
@@ -20,11 +20,15 @@
 
     <div class="history">
       <h5>历史查询</h5>
-      <nuxt-link to="#">
+      <nuxt-link
+        :to="`/air/flights?departCity=${item.departCity}&departCode=${item.departCode}&destCity=${item.destCity}&destCode=${item.destCode}&departDate=${item.departDate}`"
+        v-for="(item,index) in $store.state.air.history"
+        :key="index"
+      >
         <el-row type="flex" justify="space-between" align="middle" class="history-item">
           <div class="air-info">
-            <div class="to-from">广州 - 上海</div>
-            <p>2019-06-16</p>
+            <div class="to-from">{{item.departCity}} - {{item.destCity}}</div>
+            <p>{{item.departDate}}</p>
           </div>
           <span>选择</span>
         </el-row>
