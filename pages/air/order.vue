46d3598e2809c5cac573973783a80a61f1cd087d
砥砺前行 <1531012257@qq.com>
2019-10-17 10:45:47 +0800

    订单详情页侧边栏

--- pages/air/order.vue
+++ pages/air/order.vue
@@ -4,20 +4,41 @@
       <!-- 订单表单 -->
       <orderForm></orderForm>
       <!-- 侧边栏 -->
-      <div class="aside"></div>
+      <div class="aside">
+        <OrderAside :data="infoData"></OrderAside>
+      </div>
     </el-row>
   </div>
 </template>
 
 <script>
 import OrderForm from "@/components/air/orderForm";
+import OrderAside from "@/components/air/orderAside";
 export default {
   components: {
-    OrderForm
+    OrderForm,
+    OrderAside
   },
   data() {
     return {
+      // 定义一个机票信息
+      infoData: {
+        insurances: [], //保险数据
+        seat_infos: {} //订单信息
+      }
     };
+  },
+  // 根据ID和参数请求该航班的订单信息
+  mounted() {
+    let { id, seat_xid } = this.$route.query;
+    //   console.log(query)
+    this.$axios({
+      url: `airs/` + id,
+      params: { seat_xid }
+    }).then(res => {
+      this.infoData = res.data
+      console.log(this.infoData)
+    });
   },
 };
 </script>
