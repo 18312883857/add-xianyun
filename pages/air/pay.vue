a97db680587b97d4b50e2cbb5ab683ba5f172edd
砥砺前行 <1531012257@qq.com>
2019-10-17 18:54:16 +0800

    支付轮询（未完成）

--- pages/air/pay.vue
+++ pages/air/pay.vue
@@ -26,11 +26,45 @@
 <script>
 // 生成二维码的包
 import QRCode from "qrcode";
+import { async } from "q";
 export default {
-  data(){
-    return{
-      price : ''
+  data() {
+    return {
+      price: "",
+      timer: null
+    };
+  },
+  methods: {
+    async payment(data) {
+      let { id } = this.$route.query;
+      let {
+        air,
+        user: { userInfo }
+      } = this.$store.state;
+      return this.$axios({
+        url: `airorders/checkpay`,
+        method: "Post",
+        headers: {
+          Authorization: `Bearer ${userInfo.token}`
+        },
+        data: {
+          id, //订单id
+          nonce_str: +data.nonce_str, //订单金额
+          out_trade_no: data.out_trade_no //订单编号
+        }
+      }).then(res => {
+        let { statusTxt } = res.data;
+        console.log(res.data, statusTxt);
+        if (statusTxt === "支付完成") {
+          this.$message.success(`支付成功`);
+          return Promise.resolve(true);
+        }
+        return Promise.resolve(false);
+      });
     }
+  },
+  destroyed() {
+    clearInterval(this.timer);
   },
   mounted() {
     // 这个处理方法有缺陷  userinfo在页面中加载完才赋值
@@ -47,19 +81,30 @@ export default {
           Authorization: `Bearer ${userInfo.token}`
         }
       }).then(res => {
-        console.log(res);
         // 展示价格
-        this.price = res.data.price
+        this.price = res.data.price;
         // 生成二维码
-        let {payInfo} = res.data
+        let { payInfo } = res.data;
         // 生成二维码到canvas
         // 获取盒子id
         const stage = document.querySelector("#qrcode-stage");
         // 第一个参数是要设置二维码的位置  第二个参数生成二维码的url地址
-        QRCode.toCanvas(stage,payInfo.code_url,{width:200})
+        QRCode.toCanvas(stage, payInfo.code_url, { width: 200 });
+        // 查询接口3秒查询一次，如果已付款会弹窗提示并停止继续查询，页面销毁也会停止查询。
+        this.timer = setInterval(async () => {
+          // 调用函数并将生成二维码的对象当成参数传过去
+          let isResolve = await this.payment(payInfo);
+          // 能够打印出是否已付款的信息
+          // console.log(isResolve);
+          // 如果已付款 那么停止查询，页面销毁
+          if (isResolve) {
+            clearInterval(this.timer);
+            return;
+          }
+        }, 3000);
       });
     }, 500);
-  }
+  },
 };
 </script>
 
